55e7c961534ef0c02d24323d3a7d885a52a3517d
Christon Cardoza <Christoncardoza96@gmail.com>
2020-08-29 21:39:50 +0530

    First typescript file is added

---
+++ Basics/Helloworld.ts
@@ -0,0 +1,14 @@
+console.log("********************************************************************************");
+console.log("*                            Hello World                                       *");
+console.log("********************************************************************************");
+console.log("Hello World\n");
+
+console.log("********************************************************************************");
+console.log("*                                Note                                          *");
+console.log("********************************************************************************");
+console.log("1. Typescript is developed mainly over-come drawbacks of Javascript \n");
+console.log("2. The main usage of typescipt is help the developer during the development \n");
+console.log("3. The compiled/Transpiled typescript retuns a Javascript file \n");
+console.log("4. Typescript gives much flexibility like other Object Oriented Programming languages \n");
+console.log("5. Regardless of error thrown during compilation/transpilation it would generate Javascript file\n");
+console.log("6. To utilize more functionalities of typescript use following command \n\t tsc --init \n");
